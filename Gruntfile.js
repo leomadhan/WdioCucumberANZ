87d7f66deb3b6a2301abb2cd3fb4c96b12d2cdc7
leomadhan <madhanmohanit@gmail.com>
2021-05-06 15:43:23 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,11 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        webdriver: {
-            'test-sauce': {
-                configFile: './test/config/wdio.sauce.conf.js'
-            },
-            'test-browserstack': {
-                configFile: './test/config/wdio.browserstack.conf.js'
-            },
+        webdriver: {            
             'test-local': {
+                configFile: './test/config/wdio.local.conf.js'
+            },
+            'test': {
                 configFile: './test/config/wdio.local.conf.js'
             },
         },
@@ -17,7 +14,5 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-cucumberjs');
     grunt.loadNpmTasks('grunt-webdriver');
-    grunt.registerTask('default', ['webdriver:test-local']);
-    // grunt.registerTask('default', ['webdriver:test-sauce']);
-    // grunt.registerTask('default', ['test-browserstack']);
+    grunt.registerTask('default', ['webdriver:test-local']);  
 };
